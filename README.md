67dab7c18fd4e8e5fd55f6818010faf924dfa20e
Cem Gokmen <cem@cemgokmen.com>
2018-05-02 16:34:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 2110 GBA Games
+
+# GBA Emulator Core Used:
+
+https://github.com/taisel/IodineGBA
---
+++ user_scripts/interface.js
@@ -0,0 +1,190 @@
+var data = {
+    'Spring 2018': [
+        {
+            title: "Garden Mystery",
+            image: "",
+            studentName: "Judith Brennan",
+            summary: "",
+            slug: "garden-mystery"
+        },
+        {
+            title: "Awful Game",
+            image: "",
+            studentName: "Robert Brown",
+            summary: "",
+            slug: "awful-game"
+        },
+        {
+            title: "PatThicc",
+            image: "",
+            studentName: "Daniel Jung",
+            summary: "",
+            slug: "patthicc"
+        },
+        {
+            title: "Ravioli",
+            image: "",
+            studentName: "Stephen Kamali",
+            summary: "",
+            slug: "ravioli"
+        },
+        {
+            title: "Yeji",
+            image: "",
+            studentName: "Shannon Ke",
+            summary: "",
+            slug: "yeji"
+        },
+        {
+            title: "Frogger",
+            image: "",
+            studentName: "Tarun Maddali",
+            summary: "",
+            slug: "frogger"
+        },
+    ],
+    'Fall 2017': [
+        {
+            title: "Come Find Me",
+            image: "",
+            studentName: "Jonathan Gaul",
+            summary: "",
+            slug: "come-find-me"
+        }
+    ],
+    'TA Games': [
+        {
+            title: "Ted",
+            image: "",
+            studentName: "Austin Adams",
+            summary: "",
+            slug: "ted"
+        },
+        {
+            title: "cnake",
+            image: "",
+            studentName: "Cem Gokmen",
+            summary: "",
+            slug: "cnake"
+        },
+        {
+            title: "Bubble Bobble",
+            image: "",
+            studentName: "Madison Grams",
+            summary: "",
+            slug: "bubble-bobble"
+        },
+        {
+            title: "Super Toad-ally Mario",
+            image: "",
+            studentName: "Joshua Viszlai",
+            summary: "",
+            slug: "super-toad-ally-mario"
+        },
+        {
+            title: "Pokemon Grind Version",
+            image: "",
+            studentName: "Benjamin Yarmowich",
+            summary: "",
+            slug: "pokemon-grind"
+        },
+    ]
+};
+
+
+var PortfolioItem = React.createClass({
+    render: function () {
+        return (
+            <div className="col-sm-6 col-md-4">
+                <div className="thumbnail">
+                    <img src={"./images/" + this.props.slug + ".png"}/>
+                    <div className="caption">
+                        <h3>{this.props.title}<br/>
+                            <small>{this.props.studentName}</small>
+                        </h3>
+                        <p>{this.props.summary}</p>
+                        <p><a href={"./launcher.html#" + this.props.slug} className="btn btn-primary" role="button">Play</a></p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+var PortfolioGroup = React.createClass({
+    getInitialState: function () {
+        return ({active: true});
+    },
+    render: function () {
+        if (this.state.active) {
+            var items = this.props.data.map(function (item, i) {
+                return (
+                    [<PortfolioItem key={i} title={item.title} image={item.image} studentName={item.studentName}
+                                    summary={item.summary} slug={item.slug}></PortfolioItem>]
+                );
+            }, this);
+            return (
+                <div className="row">
+                    <h2>{this.props.title}</h2>
+                    {items}
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+})
+
+var PortfolioFilters = React.createClass({
+    updateFilter: function (name, event) {
+        var active = event.target.checked;
+        this.props.onChange(name, active);
+    },
+    render: function () {
+        var boxes = this.props.groups.map(function (group, i) {
+            return (
+                <label key={i} className="checkbox-inline"><input type="checkbox" ref={group} name={group}
+                                                                  defaultChecked={true}
+                                                                  onChange={this.updateFilter.bind(null, group)}></input>{group}
+                </label>
+            );
+        }, this);
+        return (
+            <div className="text-center">
+                <h4>Filters: </h4>
+                {boxes}
+            </div>
+        );
+    }
+})
+
+var Portfolio = React.createClass({
+    handleUpdate: function (group, active) {
+        if (this.props.data[group] != null) {
+            this.refs[group].setState({active: active})
+        }
+    },
+    render: function () {
+        var groupNames = [];
+        var groups = [];
+        for (var group in this.props.data) {
+            if (this.props.data.hasOwnProperty(group)) {
+                groupNames.push(group);
+                groups.push(<PortfolioGroup key={group} ref={group} title={group}
+                                            data={this.props.data[group]}></PortfolioGroup>)
+            }
+        }
+        return (
+            <div className="container"><PortfolioFilters groups={groupNames}
+                                                         onChange={this.handleUpdate}></PortfolioFilters>
+                <div>
+                    {groups}
+                </div>
+            </div>
+        );
+    }
+});
+
+React.render(
+    <Portfolio data={data}/>, document.getElementById("content")
+);
